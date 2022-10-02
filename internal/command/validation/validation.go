227a9793b3a442b6ff54a7f03672dc3dd3cdb89b
takumin <takumiiinn@gmail.com>
2022-10-02 18:37:14 +0900

    add includes and excludes flags for validate subcommand

--- internal/command/validation/validation.go
+++ internal/command/validation/validation.go
@@ -22,6 +22,22 @@ func NewCommands(cfg *config.Config, flags []cli.Flag) *cli.Command {
 			Value:       cfg.Path.Directory,
 			Destination: &cfg.Path.Directory,
 		},
+		&cli.StringFlag{
+			Name:        "includes",
+			Aliases:     []string{"i"},
+			Usage:       "include files extensions",
+			EnvVars:     []string{"INCLUDES"},
+			Value:       cfg.Extention.Includes,
+			Destination: &cfg.Extention.Includes,
+		},
+		&cli.StringFlag{
+			Name:        "excludes",
+			Aliases:     []string{"e"},
+			Usage:       "exclude files extensions",
+			EnvVars:     []string{"EXCLUDES"},
+			Value:       cfg.Extention.Excludes,
+			Destination: &cfg.Extention.Excludes,
+		},
 	}...)
 	return &cli.Command{
 		Name:    "validation",
